93e5f6c1dc32d189fef4face008a09394b8f1825
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-06-13 08:58:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
+function multiply(num1, num2) {
+  let num1 = 2;
+  let num2 = 31;
+  num1 * num2;
+}
 
-// Write your code here
+function random() {
+  let random = "Math.random";
+}
+
+function mod(num3, num4) {
+  let num3 = 64;
+  let num4 = 10;
+  num3 % num4;
+}
+
+function max() {
+  let max = Math.max(
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20
+  );
+}
